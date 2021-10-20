5dececfca41abb42a2d6e46f51cc602c334085a5
Billy Wang <billy.wang.aus@gmail.com>
2021-10-21 07:19:15 +1100

    Add automapper

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -1,26 +1,24 @@
 using System;
 using System.Collections.Generic;
-using API.Entities;
-using API.Extensions;
 
-namespace API.Entities
+namespace API.DTOs
 {
-    public class AppUser
+    public class MemberDto
     {
         public int Id { get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
+        public string PhotoUrl { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
         public string KnownAs { get; set; }
-        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
         public string Gender { get; set; }
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
         public string Suburb { get; set; }
         public string Street { get; set; }
-        public ICollection<Photo> Photos { get; set; }
-
+        public ICollection<PhotoDto> Photos { get; set; }
     }
 }
