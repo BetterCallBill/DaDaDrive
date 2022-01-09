62b8f4374be8d310b7befd5aa8a1684d3f7bd0d0
billy wang <billy.wang.aus@gmail.com>
2022-01-09 13:43:55 +1100

    added pagination

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Member } from 'src/app/_models/member';
+import { Pagination } from 'src/app/_models/pagination';
 import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
-  selector: 'app-member-list',
-  templateUrl: './member-list.component.html',
-  styleUrls: ['./member-list.component.css']
+	selector: 'app-member-list',
+	templateUrl: './member-list.component.html',
+	styleUrls: ['./member-list.component.css'],
 })
 export class MemberListComponent implements OnInit {
-  members$: Observable<Member[]>;
+	members: Member[];
+	pagination: Pagination;
+	pageNumber = 1;
+	pageSize = 5;
 
-  constructor(private memberService: MembersService) { }
+	constructor(private memberService: MembersService) {}
 
-  ngOnInit(): void {
-    this.members$ = this.memberService.getMembers();
-  }
+	ngOnInit(): void {
+		this.loadMembers();
+	}
+
+	// Load members from service
+	loadMembers() {
+		this.memberService.getMembers(this.pageNumber, this.pageSize).subscribe(response => {
+			this.members = response.result;
+			this.pagination = response.pagination;
+		})
+	}
 }
