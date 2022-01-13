b027cf5e6276084396b3e5c4732e5a297691d927
billy wang <billy.wang.aus@gmail.com>
2022-01-13 20:21:58 +1100

    added pagination and sort

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -53,11 +54,26 @@ namespace API.Data
 
         public async Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams)
         {
-            var query = _context.Users
-                .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-                .AsNoTracking();
+            var query = _context.Users.AsQueryable();
 
-            var result = await PagedList<MemberDto>.CreateAsync(query, userParams.PageNumber, userParams.PageSize);
+            // Exclude current user
+            query = query.Where(u => u.UserName != userParams.CurrentUsername);
+            query = query.Where(u => u.Gender == userParams.Gender);
+
+            // Filter by age
+            var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
+            var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
+
+            query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+
+            // Sort
+            query = userParams.OrderBy switch
+            {
+                "created" => query.OrderByDescending(u => u.Created),
+                _ => query.OrderByDescending(u => u.LastActive)
+            };
+
+        var result = await PagedList<MemberDto>.CreateAsync(query.ProjectTo<MemberDto>(_mapper.ConfigurationProvider).AsNoTracking(), userParams.PageNumber, userParams.PageSize);
 
             return result;
         }
---
+++ client/src/app/_models/userParams.ts
@@ -0,0 +1,13 @@
+import { User } from "./user";
+
+export class UserParams {
+    gender: string;
+    minAge = 18;
+    maxAge = 100;
+    pageSize = 6;
+    PageNumber = 1;
+
+    constructor(user: User) {
+        this.gender = user.gender;
+    }
+}
