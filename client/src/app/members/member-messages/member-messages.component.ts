14c3e073cdbef135f92f9f8a3f324479f7c45c8b
billy wang <billy.wang.aus@gmail.com>
2022-02-13 07:42:54 +1100

    send message via hub

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -14,14 +14,14 @@ export class MemberMessagesComponent implements OnInit {
 	@Input() username: string;
 	messageContent: string;
 	loading = false;
-	
+
 	constructor(public messageService: MessageService) {}
 
 	ngOnInit(): void {}
 
 	sendMessage() {
-		this.messageService.sendMessage(this.username, this.messageContent).subscribe((message) => {
-			this.messages.push(message);
+		// return Promise
+		this.messageService.sendMessage(this.username, this.messageContent).then(() => {
 			this.messageForm.reset();
 		});
 	}
