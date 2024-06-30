820b06c3db7fde7e5d9416d76ae2964e98832c41
billy wang <billy.wang.aus@gmail.com>
2024-06-30 12:52:10 +1000

    update to angular 15

--- client/src/app/_guards/admin.guard.ts
+++ client/src/app/_guards/admin.guard.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, Observable } from 'rxjs';
 import { AccountService } from '../_services/account.service';
 
 @Injectable({
@@ -10,17 +9,19 @@ import { AccountService } from '../_services/account.service';
 })
 export class AdminGuard implements CanActivate {
   constructor(private accountService: AccountService, private toastr: ToastrService) {}
-  
-  canActivate() : Observable<boolean> {
+
+  canActivate(): Observable<boolean> {
     return this.accountService.currentUser$.pipe(
       map(user => {
+        if (!user) return false;
         if (user.roles.includes('Admin') || user.roles.includes('Moderator')) {
           return true;
+        } else {
+          this.toastr.error('You cannot enter this area');
+          return false;
         }
-        
-        this.toastr.error("You cannot enter this area");
       })
-    );
+    )
   }
   
 }
